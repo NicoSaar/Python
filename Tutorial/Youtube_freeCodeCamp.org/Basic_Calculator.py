7e9ba92ecc34ce3247250ab1fe878bb3c0cb691d
Nico <Nicolas.Saar@outlook.de>
2018-12-07 21:31:26 +0100

    fix

--- Tutorial/Youtube_freeCodeCamp.org/Basic_Calculator.py
+++ Tutorial/Youtube_freeCodeCamp.org/Basic_Calculator.py
@@ -1,4 +1,5 @@
+#basic calculator
 num1 = input("Please enter the first number")
-num2 = input("Please unter the second number")
+num2 = input("Please enter the second number")
 result = float(num1) + float(num2)
 print("Addition result = " + str(result))
